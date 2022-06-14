6f3411a585435e2b9a26c399008b90b3acc311aa
Sergey Kucher <sergey@kucher.dev>
2022-06-14 18:41:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const watchEvents = (function () {
+  var gtmInit = function () {
+    window.dataLayer.push = push;
+  };
+
+  var push = function (data) {
+    window.dataLayer = window.dataLayer.concat([data]);
+    console.log('Data pushed!', data);
+  };
+
+  return {gtmInit: gtmInit};
+})();
+
+watchEvents.gtmInit();
